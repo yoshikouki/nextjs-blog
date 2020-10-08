97717c60bfe9b6369e19a8866e9a6477466fec92
yoshikouki <yoshikouki@gmail.com>
2020-10-08 22:48:06 +0900

    next.js 3.7 まで完了

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
-function Layout({ children }){
-  return <div>{ children }</div>
-}
+import styles from './layout.module.css'
 
-export default Layout
+export default function Layout({ children }){
+  return <div className={styles.container}>{ children }</div>
+}
